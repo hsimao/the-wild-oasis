4d3ca617e90b6b3a16995fa897a2338347d8ad2c
hsimao <e087754958@gmail.com>
2023-07-08 23:26:07 +0800

    feat: basic styled-component setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
+import Button from './ui/Button'
+import Input from './ui/Input'
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: red;
+  color: #333333;
+`
+
+const StyledApp = styled.div`
+  background-color: red;
+`
+
 function App() {
-  return <div>App</div>
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello</H1>
+        <Button>
+          <span>hello</span>
+        </Button>
+        <Button>Btn2</Button>
+        <Input />
+      </StyledApp>
+    </>
+  )
 }
 
 export default App
