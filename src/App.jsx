70600a430cd38cb6c031fbd5a901e0eaf164dbfa
hsimao <e087754958@gmail.com>
2023-07-09 00:16:38 +0800

    feat: Button、Row ui component style

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,39 @@ import GlobalStyles from './styles/GlobalStyles'
 import Button from './ui/Button'
 import Input from './ui/Input'
 import Heading from './ui/Heading'
+import Row from './ui/Row'
 
-const StyledApp = styled.div`
-  background-color: red;
-`
+const StyledApp = styled.div``
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Hello h1</Heading>
-        <Button>
-          <span>hello</span>
-        </Button>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello h1</Heading>
 
-        <Heading as="h2">Hello H2</Heading>
-        <Heading as="h3">Hello h3</Heading>
+            <div>
+              <Heading as="h2">Hello H2</Heading>
+              <Button>Primary medium</Button>
+              <Button variation="secondary" size="small">
+                Secondary small
+              </Button>
+            </div>
+          </Row>
 
-        <Button>Btn2</Button>
-        <Input />
+          <Row>
+            <Heading as="h3">Hello h3</Heading>
+
+            <form action="">
+              <Input />
+              <Button variation="secondary" size="small">
+                Btn2
+              </Button>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   )
