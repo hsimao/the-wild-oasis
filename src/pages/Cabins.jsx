9c295428a6d987fcac82f4c1807da62614e5e855
hsimao <e087754958@gmail.com>
2023-07-10 16:50:42 +0800

    feat: setting supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
+import { getCabins } from '../services/apiCabins'
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((res) => {
+      console.log('cabins', res)
+    })
+  }, [])
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
