4e7812fb8270fe7d101d2200c7ee5cd65882b7cc
hsimao <e087754958@gmail.com>
2023-07-18 17:37:09 +0800

    feat: Pagination component logic

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,3 +1,5 @@
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2'
+import { useSearchParams } from 'react-router-dom'
 import styled from 'styled-components'
 
 const StyledPagination = styled.div`
@@ -55,3 +57,59 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `
+
+const PAGE_SIZE = 10
+
+function Pagination({ count }) {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const currentPage = !searchParams.get('page')
+    ? 1
+    : Number(searchParams.get('page'))
+
+  const pageCount = Math.ceil(count / PAGE_SIZE)
+
+  function nextPage() {
+    const next = currentPage === pageCount ? currentPage : currentPage + 1
+    searchParams.set('page', next)
+    setSearchParams(searchParams)
+  }
+
+  function prevPage() {
+    const prev = currentPage === 1 ? currentPage : currentPage - 1
+    searchParams.set('page', prev)
+    setSearchParams(searchParams)
+  }
+
+  const showingFirstNum = (currentPage - 1) * PAGE_SIZE + 1
+  const showingLastNum =
+    currentPage === pageCount ? count : currentPage * PAGE_SIZE
+
+  if (pageCount <= 1) return null
+
+  return (
+    <StyledPagination>
+      <p>
+        Showing <span>{showingFirstNum}</span> to <span>{showingLastNum}</span>{' '}
+        of <span>{count}</span> results
+      </p>
+
+      <Buttons>
+        <PaginationButton disabled={currentPage === 1} onClick={prevPage}>
+          <HiChevronLeft />
+          <span>Previous</span>
+        </PaginationButton>
+
+        <PaginationButton
+          disabled={currentPage === pageCount}
+          onClick={nextPage}
+        >
+          <span>Next</span>
+          <HiChevronRight />
+        </PaginationButton>
+      </Buttons>
+    </StyledPagination>
+  )
+}
+
+export default Pagination
