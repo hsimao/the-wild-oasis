feb0489031e30c2a645fe99595ad5b1447b79438
hsimao <e087754958@gmail.com>
2023-07-24 18:10:34 +0800

    feat: login logic

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import Button from '../../ui/Button'
 import Form from '../../ui/Form'
 import Input from '../../ui/Input'
+import SpinnerMini from '../../ui/SpinnerMini'
 import FormRowVertical from '../../ui/FormRowVertical'
+import { useLogin } from './useLogin'
 
 function LoginForm() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('e087754958@gmail.com')
+  const [password, setPassword] = useState('mars')
 
-  function handleSubmit() {}
+  const { login, isLoading } = useLogin()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!email || !password) return
+    login({ email, password })
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -32,7 +40,9 @@ function LoginForm() {
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? 'Login' : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   )
