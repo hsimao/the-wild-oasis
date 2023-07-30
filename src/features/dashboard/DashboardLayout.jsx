25f02a786b439294527e00e6c3d3829ad3126a5e
hsimao <e087754958@gmail.com>
2023-07-30 17:35:29 +0800

    feat: recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components'
+import { useRecentBookings } from './useRecentBookings'
+import { useRecentStays } from './useRecentStays'
+import Spinner from '../../ui/Spinner'
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,11 @@ const StyledDashboardLayout = styled.div`
 `
 
 function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings()
+  const { stays, confirmedStays, isLoading: isStaysLoading } = useRecentStays()
+
+  if (isLoading || isStaysLoading) return <Spinner />
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
