5f513266085b60fc9c40e46e13dca971f71a5439
hsimao <e087754958@gmail.com>
2023-08-01 15:39:08 +0800

    feat: error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { StyleSheetManager } from 'styled-components'
+import { ErrorBoundary } from 'react-error-boundary'
 import App from './App.jsx'
+import ErrorFallback from './ui/ErrorFallback.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Fix styled-components warning https://github.com/styled-components/styled-components/issues/4049 */}
-    <StyleSheetManager shouldForwardProp={() => true}>
-      <App />
-    </StyleSheetManager>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}
+    >
+      {/* Fix styled-components warning https://github.com/styled-components/styled-components/issues/4049 */}
+      <StyleSheetManager shouldForwardProp={() => true}>
+        <App />
+      </StyleSheetManager>
+    </ErrorBoundary>
   </React.StrictMode>
 )
