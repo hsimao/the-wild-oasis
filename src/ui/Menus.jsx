4b9e5b2df289ef7827b2536d21d0d421663b4917
hsimao <e087754958@gmail.com>
2023-08-01 15:45:02 +0800

    fix: fix menus click issues

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,6 +86,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext)
 
   function handleClick(e) {
+    e.stopPropagation()
     // 往上找屬性是 button 的元素資訊
     const rect = e.target.closest('button').getBoundingClientRect()
 
@@ -112,7 +113,9 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext)
-  const ref = useOutsideClick(close)
+  const ref = useOutsideClick(() => {
+    close()
+  }, false)
 
   if (openId !== id) return null
 
